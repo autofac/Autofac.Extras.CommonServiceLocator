a4063c45744c0dfadeaefd2ec1b81c23f909c1b2
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-03 22:10:39 +1000

    Added solution and build configuration.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Autofac.Extras.CommonServiceLocator
 ===================================
 
 Common Service Locator implementation for Autofac IoC
+
+[![Build status](https://ci.appveyor.com/api/projects/status/p3hwgqspvxvmhm8n?svg=true)](https://ci.appveyor.com/project/Autofac/autofac-extras-commonservicelocator)
--- src/Autofac.Extras.CommonServiceLocator/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.CommonServiceLocator/Properties/AssemblyInfo.cs
@@ -1,5 +1,19 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 
 [assembly: AssemblyTitle("Autofac.Extras.CommonServiceLocator")]
 [assembly: CLSCompliant(true)]
+
+[assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
+[assembly: AssemblyProduct("Autofac")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
+[assembly: AssemblyDescription("Autofac.Extras.CommonServiceLocator")]
