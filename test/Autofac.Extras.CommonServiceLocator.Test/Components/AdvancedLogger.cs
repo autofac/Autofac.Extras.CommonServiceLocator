3c20c596917540b1e8f01031912b2bbb1a7ebf74
Travis Illig <tillig@paraesthesia.com>
2023-04-06 12:30:03 -0700

    Update build to .NET 7.
    - Target net7, net6, netstandard2.1, netstandard2.0: the intersection
      of what CSL and Autofac both support.
    - Build metadata (analyzers, rules, editorconfig) updated to latest.
    - Warnings resolved.

--- test/Autofac.Extras.CommonServiceLocator.Test/Components/AdvancedLogger.cs
+++ test/Autofac.Extras.CommonServiceLocator.Test/Components/AdvancedLogger.cs
@@ -2,11 +2,13 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Autofac.Extras.CommonServiceLocator.Test.Components
 {
     public class AdvancedLogger : ILogger
     {
+        [SuppressMessage("CA1303", "CA1303", Justification = "We don't need to localize strings in unit tests.")]
         public void Log(string msg)
         {
             Console.WriteLine("Log: {0}", msg);
